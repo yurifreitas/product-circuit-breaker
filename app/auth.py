768b5ac422eaded6359f6e0762d4c8dc0785209c
Yuri Bezerra Freitas <yuribzfreitas@gmail.com>
2025-05-30 14:42:42 -0300

    fix(): Adjust ident

--- app/auth.py
+++ app/auth.py
@@ -6,17 +6,16 @@ from typing import Optional
 from passlib.context import CryptContext
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi import Depends
-# Definir a chave secreta e o algoritmo para o JWT
+
 SECRET_KEY = os.getenv("SECRET_KEY")
 if not SECRET_KEY:
     raise ValueError("SECRET_KEY não está configurado no ambiente.")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60  # O token expira em 60 minutos
 
-# Definindo o esquema OAuth2
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# Função para gerar o token de acesso
 def create_access_token(data: dict, expires_delta: Optional[int] = None) -> str:
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=expires_delta) if expires_delta else datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
@@ -24,7 +23,7 @@ def create_access_token(data: dict, expires_delta: Optional[int] = None) -> str:
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-# Função para gerar o refresh token
+
 def create_refresh_token(data: dict) -> str:
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(days=7)  # Refresh token expira em 7 dias
@@ -35,7 +34,7 @@ def create_refresh_token(data: dict) -> str:
 http_bearer = HTTPBearer()
 
 def verify_token(credentials: HTTPAuthorizationCredentials = Depends(http_bearer)) -> str:
-    token = credentials.credentials  # Pega o token do cabeçalho Authorization
+    token = credentials.credentials
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
@@ -47,10 +46,8 @@ def verify_token(credentials: HTTPAuthorizationCredentials = Depends(http_bearer
     except jwt.PyJWTError:
         raise HTTPException(status_code=403, detail="Token inválido ou malformado.")
 
-# Função para verificar a senha
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-# Função para criar o hash da senha
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
--- app/middleware.py
+++ app/middleware.py
@@ -5,37 +5,35 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
 LOG_FILE_PATH = "logs/app.log"
-os.makedirs("logs", exist_ok=True)  # Garantir que o diretório de logs exista
+os.makedirs("logs", exist_ok=True)
 
 def setup_logging(app: FastAPI):
-    # Configura log padrão (Stream + File)
+
     logging.basicConfig(
-        level=logging.DEBUG,  # Nível de log: DEBUG (mostra todos os logs)
-        format="%(asctime)s - %(levelname)s - %(message)s",  # Formato de log com timestamp
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s - %(message)s",
         handlers=[
             logging.StreamHandler(),  # Exibe os logs no console
-            logging.FileHandler(LOG_FILE_PATH, mode='a', encoding='utf-8'),  # Escreve no arquivo de logs, no modo 'append'
+            logging.FileHandler(LOG_FILE_PATH, mode='a', encoding='utf-8'),
         ],
     )
 
-    logging.info("Logging foi configurado corretamente!")  # Log de teste simples
+    logging.info("Logging foi configurado corretamente!")
 
     @app.middleware("http")
     async def log_requests(request: Request, call_next):
-        start_time = time.time()  # Marca o início da requisição
+        start_time = time.time()
 
-        # Log de requisição
         logging.info(f"Requisição iniciada: {request.method} {request.url.path}")
 
         try:
-            # Processa a requisição e obtém a resposta
+
             response = await call_next(request)
         except Exception as exc:
-            # Log de erro se a requisição falhar
+
             logging.exception(f"Erro na requisição: {str(exc)}")
             return JSONResponse(status_code=500, content={"detail": "Internal Server Error"})
 
-        duration = time.time() - start_time  # Calcula a duração da requisição
-        # Log de finalização da requisição
+        duration = time.time() - start_time
         logging.info(f"Requisição finalizada: {response.status_code}, Duração: {duration:.4f}s")
         return response
