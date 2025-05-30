d373c1019d22966d15559e9b1dd33e82592ca395
Yuri Bezerra Freitas <yuribzfreitas@gmail.com>
2025-05-29 21:21:40 -0300

    feat(): ajustes nos container

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,14 @@ FROM python:3.11-slim
 
 WORKDIR /app
 
-# Instala Poetry
+# Instala o Poetry
 RUN pip install --no-cache-dir poetry
 
 COPY pyproject.toml poetry.lock* /app/
 
+# Instala dependências principais + de desenvolvimento
 RUN poetry config virtualenvs.create false \
- && poetry install --no-root --only main
+ && poetry install --no-root --with dev
 
 COPY . /app
 
--- app/main.py
+++ app/main.py
@@ -4,15 +4,21 @@ from app.clients import router as clients_router
 from app.wishlist import router as wishlist_router
 from app.middleware import setup_logging
 from prometheus_fastapi_instrumentator import Instrumentator
+from contextlib import asynccontextmanager
 
-app = FastAPI(title="Magalu Wishlist API")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+app = FastAPI(
+    title="Magalu Wishlist API",
+    debug=True,
+    lifespan=lifespan
+)
 
 setup_logging(app)
 Instrumentator().instrument(app).expose(app)
-@app.on_event("startup")
-async def startup():
-    await init_db()
-
 
 app.include_router(clients_router, prefix="/clients", tags=["clients"])
 app.include_router(wishlist_router, prefix="/wishlist", tags=["wishlist"])
